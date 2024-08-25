72a91195cc285ff17322573dad287d2c164ef8a7
ljxtmp <ljx_XueXi_temp_014@hotmail.com>
2024-08-25 11:27:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import streamlit as st
 
 def Moving_Notice():
-  st.write('# 迁移通知')
-  st.write('')
-  st.write('本站已迁移至新网址。点击下方链接以前往。')
-  st.write('')
-  col1, col2 = st.columns([1, 1])
-  with col1:
-    st.write('我的主页v0.3.5')
-    st.write('https://ljxtmpshomev03x.streamlit.app/')
-    st.button('前往 我的主页v0.3.5', 'https://ljxtmpshomev03x.streamlit.app/')
-  with col2:
-    st.write('我的工具箱v0.4.0_alpha_1')
-    st.write('https://ljxtmpstoolboxbeta.streamlit.app/')
-    st.button('前往 我的工具箱v0.4.0_alpha_1', 'https://ljxtmpstoolboxbeta.streamlit.app/')
+    st.write('# 迁移通知')
+    st.write('')
+    st.write('本站已迁移至新网址。点击下方链接以前往。')
+    st.write('')
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.write('我的主页v0.3.5')
+        st.write('https://ljxtmpshomev03x.streamlit.app/')
+        st.button('前往 我的主页v0.3.5', 'https://ljxtmpshomev03x.streamlit.app/')
+    with col2:
+        st.write('我的工具箱v0.4.0_alpha_1')
+        st.write('https://ljxtmpstoolboxbeta.streamlit.app/')
+        st.button('前往 我的工具箱v0.4.0_alpha_1', 'https://ljxtmpstoolboxbeta.streamlit.app/')
+
+def main():
+    Moving_Notice()
+
+main()
